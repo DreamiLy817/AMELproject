84ed5682d88b886463c87cef4e8d52d95587107f
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-11 16:44:00 +0200

    test fini

--- src/fr/eni/amel/dal/SectionTestDao.java
+++ src/fr/eni/amel/dal/SectionTestDao.java
@@ -1,8 +1,15 @@
 package fr.eni.amel.dal;
 
+import java.util.List;
+
 import fr.eni.amel.bo.SectionTest;
 import fr.eni.tp.web.common.dal.dao.GenericDAO;
+import fr.eni.tp.web.common.dal.exception.DaoException;
 
 public interface SectionTestDao extends GenericDAO<SectionTest, Integer> {
+	
+	public List<SectionTest> selectByTest(Integer id) throws DaoException; 
+	
+	public List<SectionTest> selectByTheme(Integer id) throws DaoException;
 	
 }
--- src/fr/eni/amel/test/dal/AppliTestDAL.java
+++ src/fr/eni/amel/test/dal/AppliTestDAL.java
@@ -2,9 +2,12 @@ package fr.eni.amel.test.dal;
 
 import java.util.List;
 
+import fr.eni.amel.bo.SectionTest;
 import fr.eni.amel.bo.Test;
+import fr.eni.amel.dal.SectionTestDao;
 import fr.eni.amel.dal.TestDao;
 import fr.eni.amel.dal.factory.DaoFactory;
+import fr.eni.amel.dal.impl.SectionTestDaoImpl;
 import fr.eni.tp.web.common.dal.exception.DaoException;
 
 public class AppliTestDAL {
@@ -17,50 +20,63 @@ public class AppliTestDAL {
 		Test test1 = new Test("zezrz", "test sur l'anglais", 60, 80, 20);
 		Test test2 = new Test("math", "test sur la langue francaise", 60, 80, 20);
 
-		//insertion des tests
+		// insertion des tests
 		System.out.println("insertion de test");
 		testDAO.insert(test1);
 		testDAO.insert(test2);
 		System.out.println(test1);
 		System.out.println(test2);
-		
-		//selection de tous les tests
+
+		// selection de tous les tests
 		List<Test> allTest = testDAO.selectAll();
-		
-		 System.out.println("---------------------------------------------------------------");
-		 System.out.println("Selection de tous les tests : ");
-		 StringBuffer sb = new StringBuffer();
-		 for (Test test : allTest) {
-		 sb.append("Test [id = ");
-		 sb.append(test.getIdTest());
-		 sb.append(", libelle = ");
-		 sb.append(test.getLibelle());
-		 sb.append("description = ");
-		 sb.append(test.getDescription()).append("]\n");
-		 }
-		 System.out.println(sb.toString());
-		 System.out.println("---------------------------------------------------------------");
-		
-		// suppression d'un test 
+
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Selection de tous les tests : ");
+		StringBuffer sb = new StringBuffer();
+		for (Test test : allTest) {
+			sb.append("Test [id = ");
+			sb.append(test.getIdTest());
+			sb.append(", libelle = ");
+			sb.append(test.getLibelle());
+			sb.append("description = ");
+			sb.append(test.getDescription()).append("]\n");
+		}
+		System.out.println(sb.toString());
+		System.out.println("---------------------------------------------------------------");
+
+		SectionTestDao sectionTestDao = DaoFactory.getSectionTestDao();
+		List<SectionTest> listeSectionTests = sectionTestDao.selectByTest(1);
+
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Selection de tous les sections de test : ");
+		StringBuffer sb1 = new StringBuffer();
+		for (SectionTest sectionTest : listeSectionTests) {
+			sb1.append(", nombre de question a tirer = ");
+			sb1.append(sectionTest.getNbQuestionsATirer()).append("]\n");
+		}
+		System.out.println(sb1.toString());
+		System.out.println("---------------------------------------------------------------");
+
+		// suppression d'un test
 		System.out.println("Suppression de l'article : " + test1.toString());
 		testDAO.delete(test1.getIdTest());
-		
+
 		allTest = testDAO.selectAll();
-		 System.out.println("---------------------------------------------------------------");
-				 System.out.println("Liste des tests après suppression : ");
-				 StringBuffer sb1 = new StringBuffer();
-				 for (Test test : allTest) {
-				 sb1.append("Test [id = ");
-				 sb1.append(test.getIdTest());
-				 sb1.append(", libelle = ");
-				 sb1.append(test.getLibelle());
-				 sb1.append("description = ");
-				 sb1.append(test.getDescription()).append("]\n");
-				 }
-				 System.out.println(sb1.toString());
-				 System.out.println("---------------------------------------------------------------");
-		
-		// mise a jour d'un test 
+		System.out.println("---------------------------------------------------------------");
+		System.out.println("Liste des tests après suppression : ");
+		StringBuffer sb11 = new StringBuffer();
+		for (Test test : allTest) {
+			sb11.append("Test [id = ");
+			sb11.append(test.getIdTest());
+			sb11.append(", libelle = ");
+			sb11.append(test.getLibelle());
+			sb11.append("description = ");
+			sb11.append(test.getDescription()).append("]\n");
+		}
+		System.out.println(sb11.toString());
+		System.out.println("---------------------------------------------------------------");
+
+		// mise a jour d'un test
 		System.out.println("mise a jour d'un test");
 		System.out.println("Avant modification :" + test2);
 		test2.setLibelle("html et css");
@@ -68,6 +84,6 @@ public class AppliTestDAL {
 		System.out.println("Apres modification : " + test2);
 		testDAO.update(test2);
 		System.out.println("mise a jour réussie");
-		
+
 	}
 }
