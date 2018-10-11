f7b734b0effe5482e3efaa9e6051f562a8440b94
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-11 15:58:27 +0200

    Base

--- src/fr/eni/amel/test/bo/AppliSectionTestBO.java
+++ src/fr/eni/amel/test/bo/AppliSectionTestBO.java
@@ -1,23 +1,22 @@
 package fr.eni.amel.test.bo;
 
-import java.util.List;
-
+import fr.eni.amel.bo.SectionTest;
 import fr.eni.amel.bo.Test;
+import fr.eni.amel.dal.SectionTestDao;
 import fr.eni.amel.dal.TestDao;
 import fr.eni.amel.dal.factory.DaoFactory;
 import fr.eni.tp.web.common.dal.exception.DaoException;
 
-public class AppliTestBO {
+public class AppliSectionTestBO {
 	public static void main(String[] args) throws DaoException {
 		// declaration et instanciation de la DAO
 
-		TestDao testDAO = DaoFactory.getTestDao();
+		SectionTestDao sectionTestDAO = DaoFactory.getSectionTestDao();
 
 		// Creer un nouveau test
-		Test test1 = new Test("zezrz", "test sur l'anglais", 60, 80, 20);
-		Test test2 = new Test("math", "test sur la langue francaise", 60, 80, 20);
+		SectionTest sectionT1 = new SectionTest(30);
 		
-		System.out.println(test1);
-		System.out.println(test2);
+		System.out.println(sectionT1);
 	}
+
 }
