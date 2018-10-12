d1e6c1959f373bc1b2fb316d62112d922187f5ad
LEMERCIER ALEXANDRE <alexandre.lemercier2017@campus-eni.fr>
2018-10-12 11:11:39 +0200

    IHM Question

--- src/fr/eni/amel/bll/manager/QuestionManager.java
+++ src/fr/eni/amel/bll/manager/QuestionManager.java
@@ -1,10 +1,13 @@
 package fr.eni.amel.bll.manager;
 
+import java.util.List;
+
 import fr.eni.amel.bo.Question;
 
 public interface QuestionManager {
 	
 	public Question getQuestionPropositions(Question question);
 	public Question getQuestion(int id);
+	public List<Question> getQuestionEpreuve(int id);
 	
 }
--- src/fr/eni/amel/bll/manager/impl/QuestionManagerImpl.java
+++ src/fr/eni/amel/bll/manager/impl/QuestionManagerImpl.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.List;
 
 import fr.eni.amel.bll.manager.QuestionManager;
+import fr.eni.amel.bo.Epreuve;
 import fr.eni.amel.bo.Proposition;
 import fr.eni.amel.bo.Question;
+import fr.eni.amel.bo.QuestionTirage;
+import fr.eni.amel.dal.EpreuveDAO;
+import fr.eni.amel.dal.QuestionDao;
 import fr.eni.amel.dal.factory.DaoFactory;
 import fr.eni.tp.web.common.dal.exception.DaoException;
 
@@ -44,6 +48,29 @@ public class QuestionManagerImpl implements QuestionManager{
 			}
 			
 			return null;
+			
+		}
+		
+		public List<Question> getQuestionEpreuve(int idEpreuve){
+			
+			List<Question> questions = new ArrayList<Question>();
+			EpreuveDAO epreuveDao = DaoFactory.epreuveDAO();
+			Epreuve epreuve = null;
+			try {
+				epreuve = epreuveDao.selectById(idEpreuve);
+			} catch (DaoException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			List<QuestionTirage> listTirage = epreuve.getlisteQuestionTirage();
+			
+			QuestionDao questionDao = DaoFactory.questionDAO();
+			
+			for(QuestionTirage laQuestion: listTirage) {
+				questions.add(laQuestion.getQuestion());		
+			}
+			
+			return questions;
 			
 		}
 }
