a10288fc8ff165d89863520c2712728af23e66a1
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-12 12:17:03 +0200

    correction factory

--- src/fr/eni/amel/test/manager/AppliPromoManager.java
+++ src/fr/eni/amel/test/manager/AppliPromoManager.java
@@ -1,12 +1,12 @@
 package fr.eni.amel.test.manager;
 
-import fr.eni.amel.bll.factory.PromotionFactory;
+import fr.eni.amel.bll.factory.ManagerFactory;
 
 public class AppliPromoManager {
 
 	public static void main(String[] args) {
 		// Test de la recherche d'une promotion 
-		PromotionFactory.promotionManager().getRecherchePromotion("Le");
+		ManagerFactory.promotionManager().getRecherchePromotion("Le");
 		
 
 	}
