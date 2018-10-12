c9cc581838d0ff9f3fc697d722646e548e8665e0
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-12 10:49:42 +0200

    creation methode recherche promotion

--- src/fr/eni/amel/dal/PromotionDao.java
+++ src/fr/eni/amel/dal/PromotionDao.java
@@ -1,8 +1,13 @@
 package fr.eni.amel.dal;
 
+import java.util.List;
+
 import fr.eni.amel.bo.Promotion;
 import fr.eni.tp.web.common.dal.dao.GenericDAO;
+import fr.eni.tp.web.common.dal.exception.DaoException;
 
 public interface PromotionDao extends GenericDAO<Promotion, Integer> {
+	
+	public List<Promotion> rechercherPromotion(String recherche) throws DaoException;
 
 }
