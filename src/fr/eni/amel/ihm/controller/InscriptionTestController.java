49af699898ae35e2dd611405f73f6a1becb5a006
Leslie Baltimore <baltimore.leslie@gmail.com>
2018-10-15 17:28:25 +0200

    page inscription test

--- src/fr/eni/amel/ihm/controller/InscriptionTestController.java
+++ src/fr/eni/amel/ihm/controller/InscriptionTestController.java
@@ -1,11 +1,17 @@
 package fr.eni.amel.ihm.controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.eni.amel.bll.factory.ManagerFactory;
+import fr.eni.amel.bo.Test;
+import fr.eni.amel.bo.Utilisateur;
 
 /**
  * Servlet implementation class InscriptionTestController
@@ -26,7 +32,12 @@ public class InscriptionTestController extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		List<Test> listeTest = ManagerFactory.testManager().getListeOfTest();
+		request.setAttribute("tests", listeTest);
+		
+		List<Utilisateur> listeCandidats = ManagerFactory.utilisateurManager().getRechercheCandidat("");
+		request.setAttribute("candidats", listeCandidats);
+		System.out.println(listeCandidats);
 		request.getRequestDispatcher("/forward/inscription-test").forward(request, response);
 	}
 
