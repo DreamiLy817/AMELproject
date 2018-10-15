75b433164bc15664b63e7dbf537416396c12b81c
GRAPIN EMMY <emmy.grapin2017@campus-eni.fr>
2018-10-15 11:31:02 +0200

    modif

--- src/fr/eni/amel/ihm/controller/TestController.java
+++ src/fr/eni/amel/ihm/controller/TestController.java
@@ -15,6 +15,8 @@ import fr.eni.amel.bll.factory.ManagerFactory;
 import fr.eni.amel.bll.manager.EpreuveManager;
 import fr.eni.amel.bo.Epreuve;
 import fr.eni.amel.bo.Question;
+import fr.eni.tp.web.common.HttpStatus;
+import fr.eni.tp.web.common.bll.exception.ManagerException;
 
 public class TestController extends HttpServlet {
 
@@ -29,8 +31,8 @@ public class TestController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-
-		// R�cup�rer l'identifiant de l'utilisateur dans la session
+		try{
+		// Récupérer l'identifiant de l'utilisateur dans la session
 		int idUtilisateur = (int) request.getSession().getAttribute("utilisateur");
 		
 		// Lister les tests pour l'utilisateur
@@ -41,24 +43,20 @@ public class TestController extends HttpServlet {
 
 		request.getRequestDispatcher("/forward/tests").forward(request, response);
 
-		// } catch (ManagerException e) {
-		// LOGGER.error("Technical Error", e);
-		// response.sendError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
-		// }
+		} catch (ManagerException e) {
+		 LOGGER.error("Technical Error", e);
+		 response.sendError(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage());
+		 }
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		String idEpreuve = request.getParameter("idEpreuve");
-		Integer id = Integer.parseInt(idEpreuve);
+	
 		String libelleEpreuve = request.getParameter("libelleEpreuve");
 		String dureeEpreuve = request.getParameter("dureeEpreuve");
 		request.setAttribute("libelleEpreuve", libelleEpreuve);
 		request.setAttribute("dureeEpreuve", dureeEpreuve);
-		// List<Question> listeQuestionsTireesAuSort =
-		// epreuveManager.tirerAuSortQuestions(id);
-		// request.setAttribute("listeQuestionsTireesAuSort",
-		// listeQuestionsTireesAuSort);
+		
 		request.getRequestDispatcher("/test/confirm").forward(request, response);
 	}
 
@@ -66,8 +64,15 @@ public class TestController extends HttpServlet {
 			throws ServletException, IOException {
 		String idEpreuve = request.getParameter("idEpreuve");
 		Integer id = Integer.parseInt(idEpreuve);
-		List<Question> listeQuestionsTireesAuSort = epreuveManager.tirerAuSortQuestions(id);
-		request.setAttribute("listeQuestionsTireesAuSort", listeQuestionsTireesAuSort);
-		request.getRequestDispatcher("/question/show").forward(request, response);
+		List<Question> listeQuestionsTireesAuSort;
+		try {
+			listeQuestionsTireesAuSort = epreuveManager.tirerAuSortQuestions(id);
+			request.setAttribute("listeQuestionsTireesAuSort", listeQuestionsTireesAuSort);
+			request.getRequestDispatcher("/question/show").forward(request, response);
+		} catch (ManagerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 }
